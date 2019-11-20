f4b8d3871b107dcc0e37ffb7baa834fb83ac88b1
eolculnamo2 <rbertram8@gmail.com>
2019-11-20 00:37:22 -0600

    add header component

--- src/main/resources/templates/lib/app_component.dart
+++ src/main/resources/templates/lib/app_component.dart
@@ -1,5 +1,6 @@
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
+import 'src/components/header/header_component.dart';
 
 import 'src/routes.dart';
 
@@ -9,7 +10,7 @@ import 'src/routes.dart';
 @Component(
   selector: 'my-app',
   templateUrl: 'app_component.html',
-  directives: [routerDirectives],
+  directives: [routerDirectives, HeaderComponent],
   exports: [RoutePaths, Routes],
 )
 class AppComponent {
---
+++ src/main/resources/templates/lib/src/components/header/header_component.dart
@@ -0,0 +1,7 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'app-header',
+  templateUrl: 'header_component.html',
+)
+class HeaderComponent {}
